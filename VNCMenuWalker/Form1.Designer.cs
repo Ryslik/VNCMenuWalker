52a22ccc05a2a7bf58030fc00aa26cd29c28149a
Ryslik <e-r-v@yandex.ru>
2012-06-16 21:23:06 +0300

    first commit

---
+++ VNCMenuWalker/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace VNCMenuWalker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 35;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 262);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "VNCMenuWalker";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ VNCMenuWalker/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace VNCMenuWalker
+{
+    public partial class Form1 : Form
+    {
+        private static Bitmap cursor = null;
+
+        public Form1()
+        {
+            cursor = VNCMenuWalker.Properties.Resources.Cursor;
+            cursor.MakeTransparent(Color.FromArgb(34, 177, 76));
+
+            InitializeComponent();
+        }
+
+
+        private static Bitmap ScreenShot(int width, int height)
+        {
+            Graphics gfxScreenshot;
+
+            Bitmap screen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            gfxScreenshot = Graphics.FromImage(screen);
+
+            Size sizeCopy = new Size(width, height);
+            gfxScreenshot.CopyFromScreen(Cursor.Position.X - (width / 2 - 2), Cursor.Position.Y - (height / 2 - 30), 0, 0, sizeCopy, CopyPixelOperation.SourceCopy);
+            gfxScreenshot.DrawImage(cursor, width / 2 - 3, height / 2 - 31);
+
+            return screen;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image = (Image)ScreenShot(this.Width, this.Height);
+        }
+    }
+}
